c87f037836d41077d8128f2ecac11ba043bb3dbe
kosi <kossyfab@gmail.com>
2024-11-23 13:44:22 +1100

    data validation pipeline created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.ds_project import logger
 from src.ds_project.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.ds_project.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -7,6 +8,17 @@ try:
     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.initiate_data_validation()
     logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
